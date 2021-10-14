c903c035f2d00875d01bcf1a851f744eddc68b81
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-10-14 13:18:20 +0530

    Improved file structure and added card component
    add component folder according to standard react file structure.
    Made one card component for displaying services of service page.

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,35 +1,26 @@
 import React from "react";
-import Card from "./Card";
+import Card from "./components/Cards/Card";
 import Sdata from "./Sdata";
 
 function Services() {
-    return (
-        <>
-            <div className="my-5 text-center">
-                <h1>Our Services</h1>
+  return (
+    <>
+      <div className='my-5 text-center'>
+        <h1>Our Services</h1>
+      </div>
+      <div className='container-fluid mb-5'>
+        <div className='row'>
+          <div className='col-10 mx-auto'>
+            <div className='row gy-4'>
+              {Sdata.map((val, id) => {
+                return <Card key={id} imgsrc={val.imgsrc} title={val.title} />;
+              })}
             </div>
-            <div className="container-fluid mb-5">
-                <div className="row">
-                    <div className="col-10 mx-auto">
-                        <div className="row gy-4">
-                            {
-                                Sdata.map((val,id)=>{
-                                    return( 
-                                        <Card
-                                            key={id}
-                                            imgsrc={val.imgsrc}
-                                            title={val.title}
-                                        />
-                                    )
-                                })
-                            }
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Services;
