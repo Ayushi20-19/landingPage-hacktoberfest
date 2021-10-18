cbabd39240555b9196d4795e326d2473359e46f1
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2021-10-18 12:39:59 +0530

    Add home component and home folder
    created a home component and home folder

---
+++ src/components/Hero/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+import Common from "./Common";
+const Home = () => {
+  return (
+    <>
+      <Common name='Welcome To Home Page' btn='Get Started' visit='/service' />
+    </>
+  );
+};
+
+export default Home;
